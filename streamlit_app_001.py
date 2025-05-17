336075b6a41a7a3a41011e7aa8b45f5ce2e86473
dbay-prog <sayaboge@gmail.com>
2025-05-16 21:58:18 -0500

    Practica2 deber completo

--- streamlit_app_001.py
+++ streamlit_app_001.py
@@ -3,12 +3,13 @@ import mlflow.sklearn
 import numpy as np
 
 st.title("Predicción de Diabetes")
+st.title("Diego Bayas")
 
 # Configurar conexión con MLflow Tracking Server
-pass
+mlflow.set_tracking_uri("http://localhost:5000")  # cambiar en función de su servidor
 
 # Cargar modelo desde el Model Registry, revise el ejemplo de flask
-pass
+model = mlflow.sklearn.load_model("models:/diabetes_9/1")  # cambiar en función de su modelo
 
 # Deslizadores para cada input del modelo
 pregnancies = st.slider("Número de embarazos", 0, 20, 1)
